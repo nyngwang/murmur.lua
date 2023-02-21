7a61c4b2bb90fa0f6a23b14c638b717439bcc9d2
nyngwang <nyngwang@gmail.com>
2023-02-22 01:25:21 +0800

    enhance(yank_blink): extend normal mode `y` (Close #12)

--- lua/murmur/utils/autocmd.lua
+++ lua/murmur/utils/autocmd.lua
@@ -1,10 +1,16 @@
 local M = {}
 ---------------------------------------------------------------------------------------------------
-local out_v = false
+-- local out_v = false
 local just_yank = false
 
 
 local function create_yank_blink()
+  -- extend `y`.
+  vim.keymap.set('n', 'y', function ()
+    local ok, m = pcall(require, 'murmur')
+    if ok then m.matchdelete() end
+    return 'y'
+  end, { expr = true })
   -- recreate `default_yank_hl`.
   vim.api.nvim_create_autocmd({ 'VimEnter', 'ColorScheme' }, {
     group = 'murmur.lua',
@@ -30,18 +36,18 @@ end
 
 local function create_murmur_cursor_rgb()
   -- pre-yanking.
-  vim.api.nvim_create_autocmd({ 'ModeChanged' }, {
-    group = 'murmur.lua',
-    pattern = '*',
-    callback = function ()
-      if
-        vim.v.event.new_mode == 'n'
-        and (vim.v.event.old_mode == 'v' or vim.v.event.old_mode == 'V' or vim.v.event.old_mode == '')
-      then
-        out_v = true
-      end
-    end
-  })
+  -- vim.api.nvim_create_autocmd({ 'ModeChanged' }, {
+  --   group = 'murmur.lua',
+  --   pattern = '*',
+  --   callback = function ()
+  --     if
+  --       vim.v.event.new_mode == 'n'
+  --       and (vim.v.event.old_mode == 'v' or vim.v.event.old_mode == 'V' or vim.v.event.old_mode == '')
+  --     then
+  --       out_v = true
+  --     end
+  --   end
+  -- })
   vim.api.nvim_create_autocmd({ 'TextYankPost' }, {
     group = 'murmur.lua',
     pattern = '*',
@@ -55,10 +61,9 @@ local function create_murmur_cursor_rgb()
     pattern = '*',
     callback = function ()
       if vim.fn.mode() ~= 'n' then return end
-      if -- just yank and out of visual mode.
-        just_yank and out_v
+      if -- move because of yank
+        just_yank
       then
-        out_v = false
         just_yank = false
         return
       end
@@ -77,7 +82,15 @@ local function create_murmur_cursor_rgb()
       if vim.v.event.new_mode == 'v'
         or vim.v.event.new_mode == 'V'
         or vim.v.event.new_mode == ''
-      then require('murmur').matchdelete() end
+      then
+        require('murmur').matchdelete()
+        return
+      end
+      if vim.v.event.new_mode == 'n'
+        and (vim.v.event.old_mode == 'v'
+        or vim.v.event.old_mode == 'V'
+        or vim.v.event.old_mode == '')
+      then vim.cmd('doau CursorMoved') end
     end
   })
   vim.api.nvim_create_autocmd({ 'WinLeave' }, {

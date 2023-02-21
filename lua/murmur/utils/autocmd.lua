dd59f6097c613cd962d0c43064028d09b128331a
nyngwang <nyngwang@gmail.com>
2023-02-22 01:57:56 +0800

    refactor: NIT

--- lua/murmur/utils/autocmd.lua
+++ lua/murmur/utils/autocmd.lua
@@ -1,6 +1,5 @@
 local M = {}
 ---------------------------------------------------------------------------------------------------
--- local out_v = false
 local just_yank = false
 
 
@@ -35,19 +34,6 @@ end
 
 
 local function create_murmur_cursor_rgb()
-  -- pre-yanking.
-  -- vim.api.nvim_create_autocmd({ 'ModeChanged' }, {
-  --   group = 'murmur.lua',
-  --   pattern = '*',
-  --   callback = function ()
-  --     if
-  --       vim.v.event.new_mode == 'n'
-  --       and (vim.v.event.old_mode == 'v' or vim.v.event.old_mode == 'V' or vim.v.event.old_mode == '')
-  --     then
-  --       out_v = true
-  --     end
-  --   end
-  -- })
   vim.api.nvim_create_autocmd({ 'TextYankPost' }, {
     group = 'murmur.lua',
     pattern = '*',
@@ -86,11 +72,6 @@ local function create_murmur_cursor_rgb()
         require('murmur').matchdelete()
         return
       end
-      if vim.v.event.new_mode == 'n'
-        and (vim.v.event.old_mode == 'v'
-        or vim.v.event.old_mode == 'V'
-        or vim.v.event.old_mode == '')
-      then vim.cmd('doau CursorMoved') end
     end
   })
   vim.api.nvim_create_autocmd({ 'WinLeave' }, {

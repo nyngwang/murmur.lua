eb49abc7eda30ee559d4229b7f72e30b1ba07434
Will Hopkins <willothyh@gmail.com>
2023-07-16 03:35:37 -0700

    feat: highlight current cursorword separately (#19)

--- lua/murmur/init.lua
+++ lua/murmur/init.lua
@@ -1,11 +1,11 @@
-local A = require('murmur.utils.autocmd')
+local A = require("murmur.utils.autocmd")
+local H = require("murmur.utils.highlight")
 local M = {}
-vim.api.nvim_create_augroup('murmur.lua', { clear = true })
+vim.api.nvim_create_augroup("murmur.lua", { clear = true })
 ---------------------------------------------------------------------------------------------------
 
 local fn = vim.fn
 local api = vim.api
-
 
 local function matchdelete(clear_word)
   if clear_word then
@@ -15,8 +15,11 @@ local function matchdelete(clear_word)
     pcall(fn.matchdelete, vim.w.cursor_word_match_id)
     vim.w.cursor_word_match_id = nil
   end
+  if vim.w.cursor_current_match_id then
+    pcall(fn.matchdelete, vim.w.cursor_current_match_id)
+    vim.w.cursor_current_match_id = nil
+  end
 end
-
 
 local function matchstr(...)
   local ok, ret = pcall(fn.matchstr, ...)
@@ -26,25 +29,28 @@ local function matchstr(...)
   return ""
 end
 
-
 -------------------------------------------------------------------------------------------------------
 function M.setup(opts)
-  if not opts then opts = {} end
+  if not opts then
+    opts = {}
+  end
 
-  M.config_cursor_rgb = opts.cursor_rgb or { guifg = 'NONE', guibg = '#393939' }
-    if type(M.config_cursor_rgb) ~= 'table' then M.config_cursor_rgb = {} end
-    if type(M.config_cursor_rgb.guifg) ~= 'string' then M.config_cursor_rgb.guifg = 'NONE' end
-    if type(M.config_cursor_rgb.guibg) ~= 'string' then M.config_cursor_rgb.guibg = '#393939' end
+  H.setup(opts.cursor_rgb, opts.cursor_rgb_current)
+
   M.yank_blink = opts.yank_blink or { enabled = true, on_yank = nil }
-    if type(M.yank_blink) ~= 'table' then M.yank_blink = {} end
-    if type(M.yank_blink.enabled) ~= 'boolean' then M.yank_blink.enabled = true end
-    if type(M.yank_blink.on_yank) ~= 'table' then
-      M.yank_blink.on_yank = {
-        higroup = 'murmur_yank_hl',
-        timeout = 200,
-        on_visual = true
-      }
-    end
+  if type(M.yank_blink) ~= "table" then
+    M.yank_blink = {}
+  end
+  if type(M.yank_blink.enabled) ~= "boolean" then
+    M.yank_blink.enabled = true
+  end
+  if type(M.yank_blink.on_yank) ~= "table" then
+    M.yank_blink.on_yank = {
+      higroup = "murmur_yank_hl",
+      timeout = 200,
+      on_visual = true,
+    }
+  end
 
   M.cursor_rgb_always_use_config = opts.cursor_rgb_always_use_config or false
   M.max_len = opts.max_len or 20
@@ -54,15 +60,21 @@ function M.setup(opts)
   M.callbacks = opts.callbacks or {}
 
   A.create_autocmds()
+  H.create_highlights()
 end
 
-
 function M.matchadd(insert_mode)
-  if vim.fn.getbufinfo(vim.fn.bufnr())[1].linecount > M.disable_on_lines then return end
-  if vim.tbl_contains(M.exclude_filetypes, vim.bo.filetype) then return end
+  if vim.fn.getbufinfo(vim.fn.bufnr())[1].linecount > M.disable_on_lines then
+    return
+  end
+  if vim.tbl_contains(M.exclude_filetypes, vim.bo.filetype) then
+    return
+  end
 
   local column = api.nvim_win_get_cursor(0)[2] + 1 -- one-based indexing.
-    if insert_mode then column = column - 1 end
+  if insert_mode then
+    column = column - 1
+  end
   local line = api.nvim_get_current_line()
 
   -- get the cursor word.
@@ -73,10 +85,12 @@ function M.matchadd(insert_mode)
   local cursor_word = left .. right
 
   -- exit when on the same cursor word.
-  if cursor_word == vim.w.cursor_word then return end
+  if cursor_word == vim.w.cursor_word then
+    return
+  end
 
   for _, cb in ipairs(M.callbacks) do
-    if type(cb) == 'function' then
+    if type(cb) == "function" then
       cb()
     end
   end
@@ -89,12 +103,19 @@ function M.matchadd(insert_mode)
     return
   end
 
-  cursor_word = fn.escape(cursor_word, [[~"\.^$[]*]])
-  vim.w.cursor_word_match_id = fn.matchadd('murmur_cursor_rgb', [[\<]] .. cursor_word .. [[\>]], -1)
+  -- highlight matches not under the cursor
+  local pat = string.format([[\(%s\)\@!\&\V\<%s\>]], [[\k*\%#\k*]], cursor_word)
+  vim.w.cursor_word_match_id = vim.fn.matchadd("murmur_cursor_rgb", pat)
+
+  -- highlight the current word under the cursor
+  pat = string.format([[%s\&\V\<%s\>]], [[\k*\%#\k*]], cursor_word)
+  vim.w.cursor_current_match_id = vim.fn.matchadd("murmur_cursor_rgb_current", pat)
 end
 
 function M.matchdelete()
-  if vim.fn.getbufinfo(vim.fn.bufnr())[1].linecount > M.disable_on_lines then return end
+  if vim.fn.getbufinfo(vim.fn.bufnr())[1].linecount > M.disable_on_lines then
+    return
+  end
   matchdelete(true)
 end
 

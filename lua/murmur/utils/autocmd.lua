e27793994bb3b919ba74871e3d6f63a968731dbe
Will Hopkins <willothyh@gmail.com>
2023-07-16 00:15:47 -0700

    feat: highlight current cursor word separately
    feat: allow highlight group linking to cursor color

--- lua/murmur/utils/autocmd.lua
+++ lua/murmur/utils/autocmd.lua
@@ -2,112 +2,107 @@ local M = {}
 ---------------------------------------------------------------------------------------------------
 local just_yank = false
 
-
 local function create_yank_blink()
-  -- recreate `default_yank_hl`.
-  vim.api.nvim_create_autocmd({ 'VimEnter', 'ColorScheme' }, {
-    group = 'murmur.lua',
-    pattern = '*',
-    callback = function ()
-      vim.cmd [[
-        hi murmur_yank_hl guifg=black guibg=#f0e130
-      ]]
-    end
-  })
-  -- main part.
-  vim.api.nvim_create_autocmd({ 'TextYankPost' }, {
-    group = 'murmur.lua',
-    pattern = '*',
-    callback = function ()
-      if not require('murmur').yank_blink.enabled then return end
-      if type(require('murmur').yank_blink.on_yank) == 'table' then
-        pcall(vim.highlight.on_yank, require('murmur').yank_blink.on_yank)
-        -- Ignore the possible `CursorMoved` after yanking and add the cursorword manually.
-        just_yank = true
-        vim.defer_fn(function ()
-          require('murmur').matchadd()
-          just_yank = false
-        end, 300)
-      end
-    end,
-  })
+	-- recreate `default_yank_hl`.
+	vim.api.nvim_create_autocmd({ "VimEnter", "ColorScheme" }, {
+		group = "murmur.lua",
+		pattern = "*",
+		callback = function()
+			vim.api.nvim_set_hl(0, "murmur_yank_hl", { fg = "black", bg = "#f0e130" })
+		end,
+	})
+	-- main part.
+	vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+		group = "murmur.lua",
+		pattern = "*",
+		callback = function()
+			if not require("murmur").yank_blink.enabled then
+				return
+			end
+			if type(require("murmur").yank_blink.on_yank) == "table" then
+				pcall(vim.highlight.on_yank, require("murmur").yank_blink.on_yank)
+				-- Ignore the possible `CursorMoved` after yanking and add the cursorword manually.
+				just_yank = true
+				vim.defer_fn(function()
+					require("murmur").matchadd()
+					just_yank = false
+				end, 300)
+			end
+		end,
+	})
 end
-
 
 local function create_murmur_cursor_rgb()
-  -- main part.
-  vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorHold' }, {
-    group = 'murmur.lua',
-    pattern = '*',
-    callback = function ()
-      if vim.fn.mode() ~= 'n' then return end
-      if -- move because of yank
-        just_yank
-      then return end
-      require('murmur').matchadd()
-    end
-  })
-  vim.api.nvim_create_autocmd({ 'CursorMovedI' }, {
-    group = 'murmur.lua',
-    pattern = '*',
-    callback = function () require('murmur').matchadd(true) end
-  })
-  vim.api.nvim_create_autocmd({ 'ModeChanged' }, {
-    group = 'murmur.lua',
-    pattern = '*',
-    callback = function ()
-      if vim.v.event.new_mode == 'v'
-        or vim.v.event.new_mode == 'V'
-        or vim.v.event.new_mode == ''
-        or (vim.v.event.new_mode == 'no' or vim.v.event.old_mode == 'no')
-      then
-        require('murmur').matchdelete()
-        return
-      end
-      if
-        vim.v.event.old_mode == 'v'
-        or vim.v.event.old_mode == 'V'
-        or vim.v.event.old_mode == ''
-      then
-        if just_yank then return end
-        require('murmur').matchadd()
-      end
-    end
-  })
-  vim.api.nvim_create_autocmd({ 'WinLeave' }, {
-    group = 'murmur.lua',
-    pattern = '*',
-    callback = function () require('murmur').matchdelete() end
-  })
-  -- try recreate `murmur_cursor_rgb` by config.
-  vim.api.nvim_create_autocmd({ 'VimEnter', 'ColorScheme' }, {
-    group = 'murmur.lua',
-    pattern = '*',
-    callback = function ()
-      if not require('murmur').cursor_rgb_always_use_config then return end
-
-      local str_template_hi_cmd = [[ hi murmur_cursor_rgb ]]
-      local config_cursor_rgb = require('murmur').config_cursor_rgb
-
-      if config_cursor_rgb.guifg then
-        str_template_hi_cmd = str_template_hi_cmd
-          .. string.format([[ guifg=%s ]], config_cursor_rgb.guifg)
-      end
-      if config_cursor_rgb.guibg then
-        str_template_hi_cmd = str_template_hi_cmd
-          .. string.format([[ guibg=%s ]], config_cursor_rgb.guibg)
-      end
-
-      vim.cmd(str_template_hi_cmd)
-    end
-  })
+	-- main part.
+	vim.api.nvim_create_autocmd({ "CursorMoved", "CursorHold" }, {
+		group = "murmur.lua",
+		pattern = "*",
+		callback = function()
+			if vim.fn.mode() ~= "n" then
+				return
+			end
+			if -- move because of yank
+				just_yank
+			then
+				return
+			end
+			require("murmur").matchadd()
+		end,
+	})
+	vim.api.nvim_create_autocmd({ "CursorMovedI" }, {
+		group = "murmur.lua",
+		pattern = "*",
+		callback = function()
+			require("murmur").matchadd(true)
+		end,
+	})
+	vim.api.nvim_create_autocmd({ "ModeChanged" }, {
+		group = "murmur.lua",
+		pattern = "*",
+		callback = function()
+			if
+				vim.v.event.new_mode == "v"
+				or vim.v.event.new_mode == "V"
+				or vim.v.event.new_mode == ""
+				or (vim.v.event.new_mode == "no" or vim.v.event.old_mode == "no")
+			then
+				require("murmur").matchdelete()
+				return
+			end
+			if vim.v.event.old_mode == "v" or vim.v.event.old_mode == "V" or vim.v.event.old_mode == "" then
+				if just_yank then
+					return
+				end
+				require("murmur").matchadd()
+			end
+		end,
+	})
+	vim.api.nvim_create_autocmd({ "WinLeave" }, {
+		group = "murmur.lua",
+		pattern = "*",
+		callback = function()
+			require("murmur").matchdelete()
+		end,
+	})
+	-- try recreate `murmur_cursor_rgb` by config.
+	vim.api.nvim_create_autocmd({ "VimEnter", "ColorScheme" }, {
+		group = "murmur.lua",
+		pattern = "*",
+		callback = function()
+			if not require("murmur").cursor_rgb_always_use_config then
+				return
+			end
+			local hl = require("murmur.utils.highlight")
+			vim.defer_fn(function()
+				hl.create_highlights()
+			end, 150)
+		end,
+	})
 end
-
 
 function M.create_autocmds()
-  create_murmur_cursor_rgb()
-  create_yank_blink()
+	create_murmur_cursor_rgb()
+	create_yank_blink()
 end
-
 
 return M
---
+++ lua/murmur/utils/highlight.lua
@@ -0,0 +1,68 @@
+local M = {}
+
+M.config = {}
+
+function M.get(hlgroup_name, attr)
+	local hlgroup_ID = vim.fn.synIDtrans(vim.fn.hlID(hlgroup_name))
+	local hex = vim.fn.synIDattr(hlgroup_ID, attr)
+	return hex ~= "" and hex or "NONE"
+end
+
+function M.setup(all, current)
+	if all then
+		M.config.all = all
+	elseif M.config.all then
+		all = M.config.all
+	end
+	if current then
+		M.config.current = current
+	elseif M.config.current then
+		current = M.config.current
+	end
+	if type(all) == table then
+		if type(all.guibg) ~= "string" then
+			all.bg = "#393939"
+		elseif all.guibg:sub(1, 1) ~= "#" and vim.fn.hlexists(all.guibg) == 1 then
+			all.bg = M.get(all.guibg, "guibg")
+		end
+		if type(all.guifg) ~= "string" then
+			all.fg = "NONE"
+		elseif all.guifg:sub(1, 1) ~= "#" and vim.fn.hlexists(all.guifg) == 1 then
+			all.fg = M.get(all.guifg, "guifg")
+		end
+	elseif type(all) == "string" and vim.fn.hlexists(all) == 1 then
+		all = { link = all }
+	else
+		all = { fg = "NONE", bg = "#393939" }
+	end
+
+	if type(current) == "table" then
+		if type(current.guibg) ~= "string" then
+			current.bg = "#393939"
+		elseif current.guibg:sub(1, 1) ~= "#" and vim.fn.hlexists(current.guibg) == 1 then
+			current.bg = M.get(current.guibg, "guibg")
+		end
+		if type(current.guifg) ~= "string" then
+			current.fg = "NONE"
+		elseif current.guifg:sub(1, 1) ~= "#" and vim.fn.hlexists(current.guifg) == 1 then
+			current.fg = M.get(current.guifg, "guifg")
+		end
+	elseif type(current) == "string" and vim.fn.hlexists(current) == 1 then
+		current = { link = current }
+	else
+		current = all
+	end
+	M.all = all
+	M.current = current
+end
+
+function M.create_highlights()
+	if M.all then
+		vim.api.nvim_set_hl(0, "murmur_cursor_rgb", M.all)
+	end
+	if M.current then
+		vim.api.nvim_set_hl(0, "murmur_cursor_rgb_current", M.current)
+	end
+end
+
+return M
